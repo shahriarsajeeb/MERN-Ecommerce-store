e5ba284a1487a37eebfb37d551cef2ac7a600423
shahriarsajeeb <sajeebshahriar665@gmail.com>
2022-05-07 22:00:11 -0700

    options expires is invalid fix done

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -6,7 +6,7 @@ const sendToken = (user,statusCode,res) =>{
     // Options for cookies
    const options = {
        expires: new Date(
-           Date.now() + process.env.COOKIE_EXPIRE + 24 * 60 * 60 * 1000
+           Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
        ),
        httpOnly: true
    };
